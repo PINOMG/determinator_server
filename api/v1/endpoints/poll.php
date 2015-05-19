579846f9b84c8bcb3be7ae138d0ab8597bf26381
Patrik <patrik.m.olsson@gmail.com>
2015-05-19 16:24:54 +0200

    Added result column to poll object

--- api/v1/endpoints/poll.php
+++ api/v1/endpoints/poll.php
@@ -15,7 +15,7 @@ function getPolls($username) {
 	if( $result == 0 )
         throw new Exception("Provided user doesn't exist", 7);
 	
-	$sql = 'SELECT poll AS id, answer, question, alternative_one, alternative_two
+	$sql = 'SELECT poll AS id, answer, question, alternative_one, alternative_two, result
 			FROM PollsAskedToUsers PATU
 			LEFT JOIN Polls P
 			ON PATU.poll = P.id
