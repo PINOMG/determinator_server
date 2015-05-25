204918e30d47f37fb985508c7b6e21fe63199cb2
Patrik <patrik.m.olsson@gmail.com>
2015-05-25 11:41:35 +0200

    Added guidelines in example connect file

--- api/v1/connect.example.php
+++ api/v1/connect.example.php
@@ -1,17 +1,14 @@
 <?php
-$dev = true;
+/**
+ * The example connect.php file to use. 
+ * Rename this file to connect.php and replace the credentials below, then you're ready to go!
+ */
 
-if($dev){
-	DEFINE('DB_HOST', 'localhost');
-	DEFINE('DB_NAME', 'pinomg');
-	DEFINE('DB_USER', 'root');
-	DEFINE('DB_PASS', 'kantarell');
-} else {
-	DEFINE('DB_HOST', 'pinomg-202715.mysql.binero.se');
-	DEFINE('DB_NAME', '202715-pinomg');
-	DEFINE('DB_USER', '202715_io35569');
-	DEFINE('DB_PASS', 'elloabbe');
-}
+//TODO: Replace credentials with those relevant to your MySQL configuration.
+DEFINE('DB_HOST', 'localhost');
+DEFINE('DB_NAME', 'pinomg');
+DEFINE('DB_USER', 'root');
+DEFINE('DB_PASS', 'password');
 
 try {
     $dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
